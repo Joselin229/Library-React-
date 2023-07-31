9875a9780ec696dd3491f46d83dcce8085d36fb3
joselin229 <jose.auqui2002@gmail.com>
2023-07-31 19:15:58 +0100

    Minor Fixes

---
+++ src/components/ui/Highlight.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Highlight = ({icon, title, subtitle}) => {
+  return (
+    <div className="highlight">
+      <div className="highlight__img">
+        {icon}
+      </div>
+      <h3 className="highlight__subtitle">{title}</h3>
+      <p className="highlight__para">
+        {subtitle}
+      </p>
+    </div>
+  );
+};
+
+export default Highlight;
